c32d46c34c075b680f61d55ccf9a7246acbacbdf
asskicker-lab <mf1732107@smail.nju.edu.cn>
2017-11-07 16:42:34 +0800

    try on my laptop

--- src/main/java/GraphxCutter.scala
+++ src/main/java/GraphxCutter.scala
@@ -1,6 +1,7 @@
+import java.nio.ByteBuffer
 import java.util.Date
 
-import GraphxDraw.{readConfig, sc}
+import GraphxDraw.{edgeMongoRDD, readConfig, sc}
 import com.mongodb.spark.MongoSpark
 import com.mongodb.spark.config.{ReadConfig, WriteConfig}
 import com.mongodb.spark.rdd.MongoRDD
@@ -27,27 +28,32 @@ object GraphxCutter {
 
   // 读取评论
   val sc = new SparkContext(sparkConf)
-  val commentsOriginalMongoRDD: MongoRDD[Document] = MongoSpark.load(sc)
 
-  val $match: Document = Document.parse("{$match: {comment_id:{$gt:1}}}")
-  val $skip: Document = Document.parse("{$skip: 10}")
-  val $limit: Document = Document.parse("{$limit: 5}")
+  def getCommentPartRDD: MongoRDD[Document] = {
 
-  val commentsPartMongoRDD: MongoRDD[Document] = commentsOriginalMongoRDD //commentsOriginalMongoRDD.withPipeline(Seq($match, $skip, $limit))
+    val commentsOriginalMongoRDD: MongoRDD[Document] = MongoSpark.load(sc)
 
-  val contentRDD: RDD[String] = commentsPartMongoRDD.map(doc => doc.get("content").asInstanceOf[String])
+    val $match: Document = Document.parse("{$match: {comment_id:{$gt:1}}}")
+    val $skip: Document = Document.parse("{$skip: 10}")
+    val $limit: Document = Document.parse("{$limit: 5}")
 
-    println("length:"+contentRDD.collect().length)
+    val commentsPartMongoRDD: MongoRDD[Document] = commentsOriginalMongoRDD //commentsOriginalMongoRDD.withPipeline(Seq($match, $skip, $limit))
+    commentsPartMongoRDD
+  }
+
+//  val contentRDD: RDD[String] = commentsPartMongoRDD.map(doc => doc.get("content").asInstanceOf[String])
+  //
+  //    println("length:"+contentRDD.collect().length)
   //  contentRDD.foreach(content => println(content))
 
   // 读取字图
   val readConfigCharacter = ReadConfig(
     Map(
-      "uri" -> ("mongodb://"+Configuration.MONGODB_HOST+":27017"),
+      "uri" -> ("mongodb://" + Configuration.MONGODB_HOST + ":27017"),
       "database" -> "jd",
-      "collection" -> "graphx_edges"))
+      "collection" -> "all_edges"))
   val characterEdgeRDD: MongoRDD[Document] = MongoSpark.load(sc, readConfigCharacter)
-  println("character:"+characterEdgeRDD.collect().length)
+  println("character:" + characterEdgeRDD.collect().length)
 
   def getCharacterPair(content: String): Array[List[String]] = {
     var characterArray = content.split("")
@@ -78,7 +84,7 @@ object GraphxCutter {
     //      characterArray
   }
 
-  def getVerticesRDD: RDD[(VertexId, Word)] = {
+  def getCommentVerticesRDD(commentsPartMongoRDD: MongoRDD[Document]): RDD[(VertexId, Word)] = {
     commentsPartMongoRDD.flatMap(comment => {
       val cid = comment.get("comment_id").asInstanceOf[Int].toLong * 1000
       val content = comment.get("content").asInstanceOf[String]
@@ -92,24 +98,28 @@ object GraphxCutter {
     })
   }
 
-  def getEdgesRDD: RDD[Edge[Link]] = commentsPartMongoRDD.flatMap(comment => {
-    val content = comment.get("content").asInstanceOf[String]
-    val cid = comment.get("comment_id").asInstanceOf[Int].toLong * 1000
-    var edgeArray = new ArrayBuffer[Edge[Link]]()
+  def getCommentEdgesRDD(commentsPartMongoRDD: MongoRDD[Document]): RDD[Edge[Link]] = {
 
-    val charArray = content.split("")
+    commentsPartMongoRDD.flatMap(comment => {
+      val content = comment.get("content").asInstanceOf[String]
+      val cid = comment.get("comment_id").asInstanceOf[Int].toLong * 1000
+      var edgeArray = new ArrayBuffer[Edge[Link]]()
 
-    var i = 0
-    for (i <- 0 until charArray.length - 1) {
-      edgeArray += Edge(cid + i, cid + i + 1, Link(1))
-    }
+      val charArray = content.split("")
 
-    edgeArray.toArray[Edge[Link]]
-  })
+      var i = 0
+      for (i <- 0 until charArray.length - 1) {
+        edgeArray += Edge(cid + i, cid + i + 1, Link(1))
+      }
+
+      edgeArray.toArray[Edge[Link]]
+    })
+  }
 
   def displayGraph(): Unit = {
-    val verticesRDD: RDD[(VertexId, Word)] = getVerticesRDD
-    val edgesRDD: RDD[Edge[Link]] = getEdgesRDD
+    val commentPartRDD = getCommentPartRDD
+    val verticesRDD: RDD[(VertexId, Word)] = getCommentVerticesRDD(commentPartRDD)
+    val edgesRDD: RDD[Edge[Link]] = getCommentEdgesRDD(commentPartRDD)
     val graph: Graph[Word, Link] = Graph(verticesRDD, edgesRDD)
     graph.cache()
 
@@ -141,8 +151,48 @@ object GraphxCutter {
   }
 
 
+  def getCharacterVerticesRDD: RDD[(VertexId, Word)] = {
+    val vertexList = List()
+    val sourceVertex = characterEdgeRDD.map(edge =>
+      edge.get("sourceName").asInstanceOf[String]
+    )
+    val desVertex = characterEdgeRDD.map(edge =>
+      edge.get("desName").asInstanceOf[String]
+    )
+    val allVertex = sourceVertex.distinct().union(desVertex.distinct()).distinct()
+    val verticesWithId: RDD[(VertexId, Word)] = allVertex.map(char => {
+      val id = ByteBuffer.wrap(char.getBytes("utf-8")).getLong
+      (id, Word(char))
+    })
+
+    verticesWithId
+  }
+
+  def getCharacterEdgesRDD: RDD[Edge[Link]] = {
+    val verticesRDD: RDD[(VertexId, Word)] = getCharacterVerticesRDD
+    val edgesRDD: RDD[Edge[Link]] = characterEdgeRDD.map(doc => {
+      val srcName = doc.get("sourceName").asInstanceOf[String]
+      val desName = doc.get("desName").asInstanceOf[String]
+      val weight = doc.get("weight").asInstanceOf[Int]
+      val srcId = ByteBuffer.wrap(srcName.getBytes("utf-8")).getLong
+      val desId = ByteBuffer.wrap(desName.getBytes("utf-8")).getLong
+      Edge(srcId, desId, Link(weight))
+    })
+
+    edgesRDD
+  }
+
+  def makeCharacterCorpusGraph: Graph[Word, Link] = {
+    val corpusVertices = getCharacterVerticesRDD
+    val corpusEdges = getCharacterEdgesRDD
+    val corpusGraph: Graph[Word, Link] = Graph(corpusVertices, corpusEdges)
+    corpusGraph
+  }
+
   def main(args: Array[String]): Unit = {
     //    displayGraph()
-
+    val corpusGraph = makeCharacterCorpusGraph
+    val edgeCount = corpusGraph.edges.collect.length
+    println("corpus edges count: " + edgeCount)
   }
 }
